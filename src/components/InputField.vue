a0bedf8f0239b3b0dc469d5b2a7ad7d10b40cc65
Ritikapatidar <patidarritika0@gmail.com>
2023-09-25 15:25:12 +0530

    updated

--- src/components/InputField.vue
+++
@@ -1,6 +0,0 @@
-<script setup>
-const props = defineProps(['modelValue', 'type'])
-</script>
-<template>
-    <input class="form-control" @input="$emit('update:modelValue', $event.target.value)" :value="modelValue" :type="type" v-bind="$attrs" />
-</template>
