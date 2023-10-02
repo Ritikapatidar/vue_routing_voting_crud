01eb8dd89a75cf71cd044c690600eac85453b504
Ritikapatidar <patidarritika0@gmail.com>
2023-10-02 18:37:12 +0530

    updated

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,18 +1,54 @@
 <script setup>
-import { ref, onMounted } from 'vue';
+import { ref, onMounted, watch, computed } from 'vue';
 import useData from '../composables/logic';
-import validations from '../composables/validation';
+import { useVuelidate } from '@vuelidate/core'
+import { required, helpers, minLength, maxLength, minValue, maxValue } from '@vuelidate/validators'
 import { useRouter } from 'vue-router';
 import { useRoute } from 'vue-router';
 import stateCity from '../assets/stateCities.json'
 
-const { validate } = validations()
 const { setLocalStorageData } = useData()
 const router = useRouter()
 const route = useRoute()
 const formData = ref({
-    Id: Math.floor(Math.random() * 9999), FirstName: '', LastName: '', DateOfBirth: '', Gender: '', Age: '', Hobbies: [], State: '', City: ''
+    Id: Math.floor(Math.random() * 9999), FirstName: '', LastName: '', DateOfBirth: '', Gender: '', Age: 0, Hobbies: [], State: '', City: ''
 })
+const rules = computed(() => ({
+    FirstName: {
+        required: helpers.withMessage("First Name Can't be Blank *", required),
+        minLength: helpers.withMessage("First Name must contain atleast 3 characters !", minLength(3)),
+        maxLength: helpers.withMessage("First Name must contain atmost 20 characters !", maxLength(20)),
+        containsOnlyCharacters: helpers.withMessage('Only Characters are allowed!', (val) => /^[A-Za-z]+$/.test(val))
+    },
+    LastName: {
+        required: helpers.withMessage("Last Name Can't be Blank *", required),
+        minLength: helpers.withMessage("Last Name must contain atleast 3 characters !", minLength(3)),
+        maxLength: helpers.withMessage("Last Name must contain atmost 20 characters !", maxLength(20)),
+        containsOnlyCharacters: helpers.withMessage('Only Characters are allowed!', (val) => /^[A-Za-z]+$/.test(val))
+    },
+    DateOfBirth: {
+        required: helpers.withMessage("Please Select Date of Birth*", required)
+    },
+    Gender: {
+        required: helpers.withMessage("Please select Gender*", required)
+    },
+    Age: {
+        required: helpers.withMessage("Age Required *", required),
+        minValue: helpers.withMessage("Not Eligible", minValue(18)),
+        maxValue: helpers.withMessage("Not Eligible", maxValue(100)),
+    },
+    Hobbies: {
+        required: helpers.withMessage("Please select hobby*", required)
+    },
+    State: {
+        required: helpers.withMessage("Please Select a State*", required),
+    },
+    City: {
+        required: helpers.withMessage("Please Select a City*", required),
+    }
+}))
+
+const v$ = useVuelidate(rules, formData.value)
 const errors = ref({
     FirstNameError: '', LastNameError: '', DOBError: '', GenderError: '', AgeError: '', HobbiesError: '', StateError: '', CityError: '', sameDataError: ''
 })
@@ -21,14 +57,29 @@ const allStates = ref([])
 const allCities = ref([])
 const edit = ref({ isEdit: false, editIndex: -1 })
 
+// Get Current date
+let cur_date = new Date().toISOString().split('T')[0]
+
+// Get Age based on DOB
+watch(()=> formData.value.DateOfBirth, () => {
+    let age = new Date().getFullYear() - new Date(formData.value.DateOfBirth).getFullYear()
+    formData.value.Age = Math.floor(age)
+}, {deep:true})
+
 onMounted(() => {
     allStates.value = Object.keys(stateCity).sort()
-    allUsers.value = JSON.parse(localStorage.getItem('userData'))
+    allUsers.value = JSON.parse(localStorage.getItem('userData1'))
     if (route.params.id) {
         let editItem = allUsers.value.find(user => user.Id === parseInt(route.params.id))
         handleStateChange(editItem.State)
+        formData.value.FirstName = editItem.FirstName
+        formData.value.LastName = editItem.LastName
+        formData.value.DateOfBirth = editItem.DateOfBirth
+        formData.value.Age = editItem.Age
+        formData.value.Hobbies = editItem.Hobbies
+        formData.value.Gender = editItem.Gender
+        formData.value.State = editItem.State
         formData.value.City = editItem.City
-        formData.value = editItem
     }
 })
 
@@ -39,11 +90,13 @@ const handleStateChange = (e) => {
 
 const resetForm = () => {
     formData.value = {
-        FirstName: '', LastName: '', DateOfBirth: '', Gender: '', Age: '', Hobbies: [], State: '', City: ''
+        FirstName: '', LastName: '', DateOfBirth: '', Gender: '', Age: "", Hobbies: [], State: '', City: ''
     }
 }
-function handleSubmit() {
-    if (validate(formData.value, errors.value)) {
+
+async function handleSubmit() {
+    let res = await v$.value.$validate()
+    if (res) {
         errors.value.sameDataError = ''
         if (route.params.id) {
             let user = allUsers.value.find((user) => user.Id === parseInt(route.params.id))
@@ -58,6 +111,7 @@ function handleSubmit() {
                 edit.value.editIndex = -1
                 edit.value.isEdit = false
                 resetForm()
+                v$.value.$reset()
                 router.push('/userData')
             }
         }
@@ -66,8 +120,8 @@ function handleSubmit() {
                 errors.value.sameDataError = "Record already exist!"
             }
             else {
-                if (localStorage.getItem('userData')) {
-                    setLocalStorageData([formData.value, ...JSON.parse(localStorage.getItem('userData'))])
+                if (localStorage.getItem('userData1')) {
+                    setLocalStorageData([formData.value, ...JSON.parse(localStorage.getItem('userData1'))])
                 }
                 else {
                     setLocalStorageData([formData.value])
@@ -76,47 +130,46 @@ function handleSubmit() {
                 router.push('/userData')
             }
         }
-        allUsers.value = JSON.parse(localStorage.getItem('userData'))
+        allUsers.value = JSON.parse(localStorage.getItem('userData1'))
     }
 }
-
-onMounted(() => {
-    allUsers.value = JSON.parse(localStorage.getItem('userData'))
-})
 </script>
 <template>
     <div class="container my-2 m-auto">
         <div class="row py-3">
             <h2 class="mx-2">Voting Registration Form</h2>
             <div class="col-12">
-                <form @submit.prevent="handleSubmit" @change="validate(formData, errors)" class="p-3 rounded border">
+                <form @submit.prevent="handleSubmit" class="p-3 rounded border">
                     <span class="formError text-danger" id="SameDataError">{{ errors.sameDataError }}</span>
                     <div class="form-group mb-3">
-                        <label for="FirstName">First Name</label>
-                        <input class="form-control" type="text" name="FirstName" id="FirstName"
+                        <label for="firstName">First Name</label>
+                        <input class="form-control" type="text" name="FirstName" id="firstName"
                             v-model="formData.FirstName" />
-                        <span class="formError text-danger" id="FirstNameError">{{ errors.FirstNameError }}</span>
+                        <span class="formError text-danger" id="FirstNameError">{{ v$.FirstName.$errors[0]?.$message
+                        }}</span>
                     </div>
                     <div class="form-group mb-3">
-                        <label for="LastName">Last Name</label>
-                        <input id="LastName" class="form-control" type="text" name="LastName" v-model="formData.LastName" />
-                        <span class="formError text-danger" id="LastNameError">{{ errors.LastNameError }}</span>
+                        <label for="lastName">Last Name</label>
+                        <input id="lastName" class="form-control" type="text" name="LastName" v-model="formData.LastName" />
+                        <span class="formError text-danger" id="LastNameError">{{ v$.LastName.$errors[0]?.$message }}</span>
                     </div>
                     <div class="row justify-content-between">
                         <div class="form-group mb-2 col-6">
-                            <label for="DOB">Date Of Birth</label>
-                            <input id="DOB" name="DOB" class="form-control" type="date" v-model="formData.DateOfBirth" />
-                            <span class="formError text-danger" id="DOBError">{{ errors.DOBError }}</span>
+                            <label for="dateOfBirth">Date Of Birth</label>
+                            <input id="dateOfBirth" :max="cur_date" name="DOB" class="form-control" type="date"
+                                v-model="formData.DateOfBirth" />
+                            <span class="formError text-danger" id="DOBError">{{ v$.DateOfBirth.$errors[0]?.$message
+                            }}</span>
                         </div>
                         <div class="form-group mb-2 col-6">
-                            <label for="Age">Age</label>
-                            <input id="Age" class="form-control" type="number" v-model="formData.Age" />
-                            <span class="formError text-danger" id="AgeError">{{ errors.AgeError }}</span>
+                            <label for="age">Age</label>
+                            <input id="age" class="form-control" type="number" disabled="true" v-model="formData.Age" />
+                            <span class="formError text-danger" id="AgeError">{{ v$.Age.$errors[0]?.$message }}</span>
                         </div>
                     </div>
                     <div class="row justify-content-between">
                         <div class="form-group mb-3 col-6">
-                            <label for="Hobbies">Hobbies</label>
+                            <label for="hobbies">Hobbies</label>
                             <div class="form-check">
                                 <input class="form-check-input" name="Hobbies" type="checkbox" v-model="formData.Hobbies"
                                     value="Chess" id="Chess">
@@ -145,41 +198,43 @@ onMounted(() => {
                                     Reading
                                 </label>
                             </div>
-                            <span class="formError text-danger" id="HobbiesError">{{ errors.HobbiesError }}</span>
+                            <span class="formError text-danger" id="HobbiesError">{{ v$.Hobbies.$errors[0]?.$message
+                            }}</span>
                         </div>
                         <div class="form-group mb-2 col-6">
                             <label for="Gender">Gender</label>
                             <div class="form-check">
-                                <input class="form-check-input" id="Male" name="Gender" value="Male" type="radio"
-                                    v-model="formData.Gender" /><label class="form-check-label" for="Male">Male</label>
+                                <input class="form-check-input" id="male" name="Gender" value="Male" type="radio"
+                                    v-model="formData.Gender" /><label class="form-check-label" for="male">Male</label>
                             </div>
                             <div class="form-check">
-                                <input class="form-check-input" id="Female" name="Gender" value="Female" type="radio"
-                                    v-model="formData.Gender" /><label class="form-check-label" for="Female">Female</label>
+                                <input class="form-check-input" id="female" name="Gender" value="Female" type="radio"
+                                    v-model="formData.Gender" /><label class="form-check-label" for="female">Female</label>
                             </div>
-                            <span class="formError text-danger" id="GenderError">{{ errors.GenderError }}</span>
+                            <span class="formError text-danger" id="GenderError">{{ v$.Gender.$errors[0]?.$message }}</span>
                         </div>
                     </div>
 
                     <div class="form-group mb-3">
-                        <label for="State">Select a State</label>
-                        <select class="form-select" v-model="formData.State"
+                        <label for="stateDropdown">Select a State</label>
+                        <select class="form-select" id="stateDropdown" v-model="formData.State"
                             @change="handleStateChange($event.target.value)">
                             <option value="" disabled selected>Select a State</option>
                             <option :value="state" v-for="state in allStates">{{ state }}</option>
                         </select>
-                        <span class="formError text-danger" id="StateError">{{ errors.StateError }}</span>
+                        <span class="formError text-danger" id="StateError">{{ v$.State.$errors[0]?.$message }}</span>
                     </div>
                     <div class="form-group mb-3">
-                        <label for="City">Select a City</label>
-                        <select class="form-select" v-model="formData.City">
+                        <label for="cityDropdown">Select a City</label>
+                        <select class="form-select" id="cityDropdown" v-model="formData.City">
                             <option value="" selected disabled>Select a City</option>
                             <option :value="city" v-for="city in allCities">{{ city }}</option>
                         </select>
-                        <span class="formError text-danger" id="CityError">{{ errors.CityError }}</span>
+                        <span class="formError text-danger" id="CityError">{{ v$.City.$errors[0]?.$message }}</span>
                     </div>
 
-                    <button id="btnSubmit" class="btn btn-success text-light">{{ !route.params.id ? 'Add' : 'Update'
+                    <button :id="route.params.id ? 'update' : 'submit'" class="btn btn-success text-light">{{
+                        !route.params.id ? 'Add' : 'Update'
                     }}</button>
                 </form>
             </div>
--- src/components/UserData/Listing.vue
+++ src/components/UserData/Listing.vue
@@ -2,36 +2,64 @@
 import { ref, onMounted } from 'vue';
 import ConfirmModal from '../ConfirmModal.vue'
 import useData from '../../composables/logic';
-import InputComponent from '../InputComponent.vue';
-const props = defineProps(['data', 'handleEdit'])
+import stateCity from '../../assets/stateCities.json'
+import "vue-search-select/dist/VueSearchSelect.css"
+
+defineProps(['data', 'handleEdit'])
 const isDeleteModal = ref(false)
 const deleteIndex = ref(null)
 const allUsers = ref([])
 const newUserData = ref([])
 const hobbiesArr = ['Chess', 'Badminton', 'Hockey', 'Reading']
-const { setLocalStorageData } = useData()
-const search = ref({ city: '', state: '', gender: '', hobbies: [], startAge: 0, MaxAge: 0 })
+const { setLocalStorageData, sortData } = useData()
+const search = ref({ name: '', city: '', state: '', gender: '', hobbies: [], startAge: 0, MaxAge: 0 })
+const cityList = ref([])
+const stateList = ref([])
+
 onMounted(() => {
-    allUsers.value = JSON.parse(localStorage.getItem('userData'))
+    allUsers.value = JSON.parse(localStorage.getItem('userData1'))
     newUserData.value = allUsers.value
-    let ages = allUsers.value.map((i) => i.Age)
+
+    // Sort Data
+    sortData(newUserData.value)
+
+    // Get Maximum Age
+    let ages = allUsers.value?.map((i) => i.Age)
     search.value.MaxAge = Math.max.apply(0, ages)
+    search.value.startAge = search.value.MaxAge
+
+    // Set state list
+    Object.keys(stateCity).map((state) => {
+        stateList.value.push({ value: state, text: state })
+    })
+
+    // Set city list
+    Object.values(stateCity).map((c) => {
+        cityList.value.push(...c)
+    })
 })
+
+// Open Delete Modal
 const handleDeleteModal = (key) => {
     isDeleteModal.value = true
     deleteIndex.value = key
 }
-const handleDelete = (index) => {
-    allUsers.value = allUsers.value.filter((user) => allUsers.value.indexOf(user) !== index)
+// Delete Data
+const handleDelete = (id) => {
+    let d = allUsers.value.filter((user) => user.Id !== id)
+    allUsers.value = d
+    newUserData.value = d
     setLocalStorageData(allUsers.value)
 }
+// Close Delete Modal
 const closeDeleteModal = () => {
     isDeleteModal.value = false
 }
 
-const handleSearch = (e) => {
+const handleSearch = () => {
     newUserData.value = allUsers.value.filter((user) => {
         if (
+            (search.value.name.length === 0 ? true : (user.FirstName.toUpperCase() + " " + user.LastName.toUpperCase()).includes(search.value.name.toUpperCase())) &&
             (search.value.city.length === 0 ? true : user.City.toUpperCase().includes(search.value.city.toUpperCase())) &&
             (search.value.state.length === 0 ? true : user.State.toUpperCase().includes(search.value.state.toUpperCase())) &&
             (search.value.gender.length === 0 ? true : user.Gender.toUpperCase() === search.value.gender.toUpperCase()) &&
@@ -48,15 +76,22 @@ const handleSearch = (e) => {
     <div class="container m-auto pt-4 pb-5" v-if="allUsers?.length > 0">
         <div class="row my-2">
             <div class="col-12 col-sm-6 col-lg-3 my-1">
-                <InputComponent type="text" v-model="search.state" name="state" id="state" @input="handleSearch($event)"
-                    placeholder="Search Based on state.." />
+                <input v-model="search.name" type="text" class="form-control" placeholder="Search by Name.." />
             </div>
             <div class="col-12 col-sm-6 col-lg-3 my-1">
-                <InputComponent type="text" v-model="search.city" name="city" id="city" @input="handleSearch($event)"
-                    placeholder="Search Based on city.." />
+                <select class="slectpicker form-select" v-model="search.state">
+                    <option selected value="">Search by State</option>
+                    <option v-for="state in Object.keys(stateCity).sort()" :value="state">{{ state }}</option>
+                </select>
             </div>
             <div class="col-12 col-sm-6 col-lg-3 my-1">
-                <select class="form-select" name="gender" v-model="search.gender" @change="handleSearch($event)">
+                <select class="slectpicker form-select" v-model="search.city">
+                    <option selected value="">Search by City</option>
+                    <option v-for=" city in cityList.sort()" :value="city">{{ city }}</option>
+                </select>
+            </div>
+            <div class="col-12 col-sm-6 col-lg-3 my-1">
+                <select class="form-select" name="gender" v-model="search.gender">
                     <option value="">Search Based on gender</option>
                     <option value="male">Male</option>
                     <option value="female">Female</option>
@@ -67,7 +102,7 @@ const handleSearch = (e) => {
                     <label for="hobbies" class="p-0">Hobbies</label>
                     <div class="form-check col-6 col-sm-3" v-for="hobby in hobbiesArr">
                         <input class="form-check-input" name="hobbies" type="checkbox" v-model="search.hobbies"
-                            @change="handleSearch($event)" :value="hobby" :id="hobby">
+                            :value="hobby" :id="hobby">
                         <label class="form-check-label" :for="hobby">
                             {{ hobby }}
                         </label>
@@ -78,13 +113,15 @@ const handleSearch = (e) => {
                 <span class="d-block text-center"><strong> Age: </strong>{{ search.startAge }}</span>
                 <div class="d-flex">
                     0<input type="range" id="to" class="form-range" v-model="search.startAge" min="0"
-                        :max="search.MaxAge" @input="handleSearch($event)" />{{ search.MaxAge }}
+                        :max="search.MaxAge" />{{ search.MaxAge }}
                 </div>
             </div>
+            <div class="col-12 col-sm-6 col-lg-3 my-1">
+                <button @click="handleSearch" class="btn btn-success border ms-auto d-block">Filter</button>
+            </div>
         </div>
-
         <div class="table-responsive text-nowrap">
-            <table id="tblOutput" class="table table-striped table-bordered">
+            <table id="tblOutput" class="table table-striped table-bordered table-hover">
                 <thead>
                     <tr>
                         <template v-for="(value, key) of newUserData[0]">
@@ -107,9 +144,12 @@ const handleSearch = (e) => {
                             </td>
                         </template>
                         <td><router-link :to="`/userData/edit/${value.Id}`" class="btn btn-success"
-                                style="font-size: 14px;"> Edit<i class="bi bi-pencil-fill"></i></router-link>
+                                :id="`${value.FirstName}-${value.LastName}-edit`" style="font-size: 14px;"> Edit<i
+                                    class="bi bi-pencil-fill ms-1"></i></router-link>
                         </td>
-                        <td><button class="btn btn-danger" style="font-size: 14px" @click="handleDeleteModal(key)">Delete
+                        <td><button class="btn btn-danger" style="font-size: 14px"
+                                :id="`${value.FirstName}-${value.LastName}-delete`"
+                                @click="handleDeleteModal(value.Id)">Delete
                                 <i class="bi bi-trash"></i></button></td>
                     </tr>
                 </tbody>
