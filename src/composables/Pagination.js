ca502c0c3c53c261f29e0e8e56fb6cb4f1ad8205
Ritikapatidar <patidarritika0@gmail.com>
2023-10-03 18:28:20 +0530

    update:Bug fixed

--- src/composables/Pagination.js
+++ src/composables/Pagination.js
@@ -5,13 +5,13 @@ export default async function Pagination(arr, size) {
     const totalPages = ref(0)
     const newPaginationData = ref([])
     pageSize.value = size
-    totalPages.value = Math.ceil(arr.length / pageSize.value)
+    totalPages.value = Math.ceil(arr?.length / pageSize.value)
     const updatePage = (page) => {
         curPage.value = page
         updateData();
     }
     const updateData = () => {
-        newPaginationData.value = arr.slice(curPage.value * pageSize.value, (curPage.value * pageSize.value) + pageSize.value)
+        newPaginationData.value = arr?.slice(curPage.value * pageSize.value, (curPage.value * pageSize.value) + pageSize.value)
     }
     updateData()
     return { curPage, totalPages, pageSize, newPaginationData, updateData, updatePage }
