656c750c395977b5834ec9ea108f2c66f0a97adf
Ritikapatidar <patidarritika0@gmail.com>
2023-10-04 18:32:25 +0530

    variable name changed

--- src/components/UserData/TableHeader.vue
+++ src/components/UserData/TableHeader.vue
@@ -1,15 +1,15 @@
 <script setup>
 import { ref } from 'vue';
 const props = defineProps(['type', 'sortDataField'])
-const toggle = ref(false)
-const handleToggle = () => {
-    toggle.value = !toggle.value
-    props.sortDataField(props.type, toggle.value)
+const sortVal = ref(false)
+const handleSorting = () => {
+    sortVal.value = !sortVal.value
+    props.sortDataField(props.type, sortVal.value)
 }
 </script>
 <template>
-    <p className="d-inline" :id="`srt-${type}`" @click="handleToggle">
-        <i class="bi bi-arrow-down" v-if="toggle"></i>
+    <p className="d-inline" :id="`srt-${type}`" @click="handleSorting">
+        <i class="bi bi-arrow-down" v-if="sortVal"></i>
         <i class="bi bi-arrow-up" v-else></i>
     </p>
 </template>
